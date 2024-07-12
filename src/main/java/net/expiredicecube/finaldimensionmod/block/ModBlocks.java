009d503c9c137cc9ca854eb7765f27d416c78312
Nchiza <nchiza@hawk.iit.edu>
2024-07-11 20:15:16 -0500

    Added: Exposed Wire North Model Implemented

--- src/main/java/net/expiredicecube/finaldimensionmod/block/ModBlocks.java
+++ src/main/java/net/expiredicecube/finaldimensionmod/block/ModBlocks.java
@@ -1,15 +1,14 @@
 package net.expiredicecube.finaldimensionmod.block;
 
 import net.expiredicecube.finaldimensionmod.FinalDimensionMod;
+import net.expiredicecube.finaldimensionmod.block.custom.WireBlock;
 import net.expiredicecube.finaldimensionmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,7 +21,7 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, FinalDimensionMod.MOD_ID);
 
     public static final RegistryObject<Block> EXPOSED_WIRE_WIRE = BLOCKS.register("exposed_wire_wire",
-            () -> new RedStoneWireBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_WIRE).noCollission()));
+            () -> new WireBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_WIRE).noCollission().noOcclusion()));
 
     public static final RegistryObject<Block> GLISTENING_OBSIDIAN = registerBlock("glistening_obsidian",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.OBSIDIAN).sound(SoundType.GILDED_BLACKSTONE)));
